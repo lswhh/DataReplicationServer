a3d2733be1b97bd98f93031f5fb496d5ebaa7f0f
lswhh <lsw0075@gmail.com>
2022-04-25 21:31:35 +0900

    mysql 테스트 케이스 추가
    추가

---
+++ test/kr/datareplicationserver/testMariadb.java
@@ -0,0 +1,83 @@
+package kr.datareplicationserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class testMariadb {
+
+	@Test
+	void test() {
+		final String driver = "org.mariadb.jdbc.Driver";
+		final String DB_IP = "localhost";
+		final String DB_PORT = "3306";
+		final String DB_NAME = "testdb";
+		final String DB_URL = 
+				"jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(DB_URL, "testuser", "manager");
+			if (conn != null) {
+				System.out.println("DB 접속 성공");
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DB 접속 실패");
+			e.printStackTrace();
+		}
+
+		try {
+			String sql = "select * from tablea";
+
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+			String userId = null;
+			String password = null;
+			String name = null;
+			while (rs.next()) {
+				userId = rs.getString(1);
+//				password = rs.getString(2);
+//				name = rs.getString(3);
+			}
+
+			System.out.println(userId);
+//			System.out.println(password);
+//			System.out.println(name);
+
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		fail("Not yet implemented");
+	}
+
+}
